ad0ab12373c888555524ffb41761f1ad88d8d949
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2023-01-28 15:53:25 +0100

    Setting up some tests for users list

---
+++ tests/users/list.test.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Go to the URL', () => {
+  test.beforeEach(async ({ page }) => await page.goto('https://discover-next-js.vercel.app/users'));
+
+  test('has title', async ({ page }) => await expect(page).toHaveTitle(/List of Users/));
+
+  test('user detail page first link', async ({ page }) => {
+    const firstLink = page.locator('text=detail').first();
+    await expect(firstLink).toHaveAttribute('href', '/users/1');
+  });
+
+  test.describe('navigation on the first user detail page', () => {
+    test.beforeEach(async ({ page }) => {
+      const firstLink = page.locator('text=detail').first();
+      await firstLink.click();
+    });
+
+    test('user detail page URL', async ({ page }) => await expect(page).toHaveURL(/1/));
+
+    test('user detail page content', async ({ page }) => {
+      await expect(page.locator('text=Leanne Graham')).toBeVisible();
+      await expect(page.locator('text=Romaguera-Crona')).toBeVisible();
+      await expect(page.locator('text=hildegard.org')).toBeVisible();
+      await expect(page.locator('text=Sincere@april.biz')).toBeVisible();
+    });
+
+    test('go back button', async ({ page }) =>
+      await expect(page.locator('text=Go Back')).toBeVisible());
+  });
+});
