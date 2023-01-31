6712810b1c69291c7c4b71e1ac07ed55d964ec10
mena0018 <rabie.menad@etudiant.univ-reims.r>
2023-01-31 16:47:27 +0100

    Dark theme only

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from 'components/Navbar';
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang='en'>
+    <html lang='en' data-theme='dark'>
       <body>
         <Navbar />
         {children}
